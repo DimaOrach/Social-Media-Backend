aa888ef0f5df5f33c163105853b4a45b30b951f4
DimaOrach1 <DimaOrach1@example.com>
2024-02-19 13:46:10 +0200

    PORT and mongoose

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Test!!');
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+try {
+    mongoose.connect('mongodb+srv://dmytroorach:lord21153@cluster0.atpdsa7.mongodb.net/Blog?retryWrites=true&w=majority')
+    app.listen(5000, ()=> console.log('SERVER IS WORKING ON PORT 5000'));
+} catch(error) {
+    console.log(error);
+}
