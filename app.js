67f86e734497916ff35d6c86cb0469c55e732ab2
DimaOrach1 <DimaOrach1@example.com>
2024-02-21 22:04:47 +0200

    models, routes, controlles

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import router from './routes/user-routes.js';
+
+
 
 const app = express();
+app.use('/api/user', router)
+
 
 try {
     mongoose.connect('mongodb+srv://dmytroorach:lord21153@cluster0.atpdsa7.mongodb.net/Blog?retryWrites=true&w=majority')
