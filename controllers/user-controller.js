a42b4cbef14405ddf1b54bc0490d4ecf073ef139
DimaOrach1 <DimaOrach1@example.com>
2024-02-22 21:38:15 +0200

    Added log in

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const getAllUsers = async (req, res, next) => {
     let users;
@@ -26,10 +27,12 @@ export const signUp = async (req, res, next) => {
             .status(400)
             .json({ message: 'User already exist! Login instead' });
     }
+    const hashedPassword = bcrypt.hashSync(password);
+
     const user = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
     });
 
     try {
@@ -38,4 +41,25 @@ export const signUp = async (req, res, next) => {
         return console.log(error);
     }
     return res.status(201).json({ user });
+};
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (error) {
+        return console.log(error);
+    }
+    if (!existingUser) {
+        return res
+            .status(404)
+            .json({ message: 'Couldnt Find User By This Email' });
+    } 
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if(!isPasswordCorrect) {
+        return res.status(404).json({message: 'Incorrect Password'});
+    }
+    return res.status(200).json({message: 'Login Successfull!'});
 };
