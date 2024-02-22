ca22a2abea203b57729363d338437f3cdb1eeda6
DimaOrach1 <DimaOrach1@example.com>
2024-02-22 22:53:54 +0200

    blog router controller

---
+++ controllers/blog-controller.js
@@ -0,0 +1,40 @@
+import Blog from '../models/Blog.js';
+
+export const getAllBlogs = async (req, res, next) => {
+    let blogs;
+    try {
+        blogs = await Blog.find();
+    } catch (error) {
+        console.log(error);
+    }
+    if (!blogs) {
+        return res.status(404).json({ message: 'Blogs Not Found' });
+    }
+    return res.status(200).json({ blogs });
+};
+
+export const addBlog = async (req, res, next) => {
+    const { title, description, image, user } = req.body;
+    const blog = new Blog({ title, description, image, user });
+    try {
+        await blog.save();
+    } catch (error) {
+        return console.log(error);
+    }
+    return res.status(200).json({ blog });
+};
+
+export const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        blog = await Blog.findByIdAndUpdate(blogId, { title, description });
+    } catch (error) {
+        return console.log(error);
+    }
+    if (!blog) {
+        return res.status(500).json({ message: 'Unable To Update The Blog' });
+    }
+    return res.status(200).json({ blog });
+};
